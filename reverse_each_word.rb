8bb88440e94d34458df7515e2a2eca429def381e
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 01:06:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new = string.split
   reversed = new.collect {|w| w.reverse}
-  resversed.concat
+  resversed.join(" ")
 end
 
 pp reverse_each_word("Hello there, and how are you?")
