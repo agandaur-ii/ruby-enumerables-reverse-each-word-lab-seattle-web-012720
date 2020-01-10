044060447a19aec43e717cdcfd579390335dc143
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 00:52:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
 def reverse_each_word(string)
-  new = []
-  new << string
-  puts "========"
-  pp new
-  puts "========"
-  new.collect {|w| w.reverse}
-  puts "********"
-  pp new
-  puts "********"
-  new[0]
+  string.reverse
 end
 
 puts reverse_each_word("Hello there, and how are you?")
