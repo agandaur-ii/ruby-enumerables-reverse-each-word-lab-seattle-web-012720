fa5bc5038c204259c1d2bba7b13732896498a3f7
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 00:45:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   new = []
   new << string
-  new.each(|w| w.reverse)
+  new.each{|w| w.reverse)}
+  new[0]
 end
 
 puts reverse_each_word("Hello there, and how are you?")
